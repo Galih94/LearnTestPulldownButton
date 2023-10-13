a00951216a37fc93eb7e0ce1bcd922430c879fe6
galih <samudragalih94@gmail.com>
2023-10-13 14:27:01 +0700

    feat(MainViewControllerTests): add test on did load configure button

--- LearnTestPulldownButton/LearnTestPulldownButtonTests/MainViewControllerTests.swift
+++ LearnTestPulldownButton/LearnTestPulldownButtonTests/MainViewControllerTests.swift
@@ -6,7 +6,29 @@
 //
 
 import XCTest
+import LearnTestPulldownButton
 
 final class MainViewControllerTests: XCTestCase {
 
+    func test_initConfigureButton_currentButtonTitleSelectedFirstTitle() {
+        let sut = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.currentButtonTitleSelected, "button 1")
+    }
+    
+    //MARK: Helpers
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> MainViewController {
+        let sut = MainViewController(nibName: "MainViewController", bundle: Bundle(for: MainViewController.self))
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+    
+    func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have beed deallocated, potential memory leak", file: file, line: line)
+        }
+    }
 }
